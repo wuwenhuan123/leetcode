d15cfc0ed60399d1c5e0dafe191ba1c77d7ed3bc
wwh <wwh960817@163.com>
2024-11-13 00:59:13 +0800

    完成题目1，两数之和

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Arrays;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!111222");
+        int[] nums = {2, 5, 5, 11};
+        int target = 10;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+
+    /**
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+     *
+     * @param nums   输入数组
+     * @param target 目标之和
+     * @return 返回数组
+     */
+    static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = nums.length - 1; j > i; j--) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
     }
 }
