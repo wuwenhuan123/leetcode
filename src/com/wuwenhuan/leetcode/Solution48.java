5ec04cc9eb28273e35d08f9353baf25f8186b846
wwh <wwh960817@163.com>
2024-11-21 21:52:26 +0800

    完成题目48

---
+++ src/com/wuwenhuan/leetcode/Solution48.java
@@ -0,0 +1,59 @@
+package com.wuwenhuan.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+ * <p>
+ * 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+ */
+public class Solution48 {
+
+    /**
+     * 方法一，原地旋转，参考官方教程
+     */
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < (n + 1) / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = temp;
+            }
+        }
+    }
+
+
+    /**
+     * 方法二，先做上下翻转，再做对角线翻转，即可达到旋转的目的
+     */
+    public void rotate2(int[][] matrix) {
+        int n = matrix.length;
+        // 水平翻转
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n; j++) {
+                int temp = matrix[i][i];
+                matrix[i][j] = matrix[n - 1 - i][j];
+                matrix[n - 1 - i][j] = temp;
+            }
+        }
+
+        // 主对角线翻转
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        Solution48 solution48 = new Solution48();
+        solution48.rotate2(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+    }
+}
