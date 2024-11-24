a1d1edaefcdef56a375440b0247957c3357d6dbc
wwh <wwh960817@163.com>
2024-11-24 23:44:59 +0800

    完成题目14

---
+++ src/com/wuwenhuan/leetcode/Solution14.java
@@ -0,0 +1,142 @@
+package com.wuwenhuan.leetcode;
+
+public class Solution14 {
+
+
+    /**
+     * 方法一：横向扫描，用LCP(S1...Sn）表示字符串S1...Sn的最长公共前缀。
+     * 可以得到以下结论：
+     * LCP(S1... Sn) = LCP(LCP(LCP(S1, S2), S3),... Sn)
+     */
+    public String longestCommonPrefix(String[] strs) {
+
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        int count = strs.length;
+        for (int i = 1; i < count; i++) {
+            //两个两个字符串求最长公共前缀
+            prefix = longestCommonPrefix(prefix, strs[i]);
+            if (prefix.length() == 0) {
+                break;
+            }
+        }
+        return prefix;
+
+    }
+
+    /**
+     * 两个字符串的最长公共前缀
+     */
+    public String longestCommonPrefix(String str1, String str2) {
+        int length = Math.min(str1.length(), str2.length());
+        int index = 0;
+        while (index < length && str1.charAt(index) == str2.charAt(index)) {
+            index++;
+        }
+        return str1.substring(0, index);
+    }
+
+    /**
+     * 方法二：纵向扫描
+     */
+    public String longestCommonPrefix2(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int length = strs[0].length();
+        int count = strs.length;
+        for (int i = 0; i < length; i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < count; j++) {
+                //按顺序比较每一个字符，如果超过其中一个字符串长度就退出
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+
+    /**
+     * 方法三：分治法，结合方法一，将问题拆开成多个小部分，加上递归
+     */
+    public String longestCommonPrefix3(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        } else {
+            return longestCommonPrefix(strs, 0, strs.length - 1);
+        }
+    }
+
+    public String longestCommonPrefix(String[] strs, int start, int end) {
+        if (start == end) {
+            return strs[start];
+        } else {
+            int mid = (end - start) / 2 + start;
+            String lcpLeft = longestCommonPrefix(strs, start, mid);
+            String lcpRight = longestCommonPrefix(strs, mid + 1, end);
+            return commonPrefix(lcpLeft, lcpRight);
+        }
+    }
+
+    public String commonPrefix(String lcpLeft, String lcpRight) {
+        int minLength = Math.min(lcpLeft.length(), lcpRight.length());
+        for (int i = 0; i < minLength; i++) {
+            if (lcpLeft.charAt(i) != lcpRight.charAt(i)) {
+                return lcpLeft.substring(0, i);
+            }
+        }
+        return lcpLeft.substring(0, minLength);
+    }
+
+
+    /**
+     * 方法四：二分查找
+     */
+    public String longestCommonPrefix4(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int minLength = Integer.MAX_VALUE;
+        for (String str : strs) {
+            minLength = Math.min(minLength, str.length());
+        }
+        int low = 0, high = minLength;
+        while (low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            if (isCommonPrefix(strs, mid)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return strs[0].substring(0, low);
+    }
+
+    public boolean isCommonPrefix(String[] strs, int length) {
+        String str0 = strs[0].substring(0, length);
+        int count = strs.length;
+        for (int i = 1; i < count; i++) {
+            String str = strs[i];
+            for (int j = 0; j < length; j++) {
+                if (str0.charAt(j) != str.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        String[] str = {"flower", "flow", "flight"};
+        Solution14 solution14 = new Solution14();
+        System.out.println(solution14.longestCommonPrefix(str));
+
+    }
+}
--- src/com/wuwenhuan/leetcode/Solution498.java
+++ src/com/wuwenhuan/leetcode/Solution498.java
@@ -11,7 +11,7 @@ public class Solution498 {
         int col = matrix[0].length;
         int[] result = new int[row * col];
         int pos = 0;
-        for (int i = 0; i < row + col - 2; i++) {
+        for (int i = 0; i < row + col - 1; i++) {
             if (i % 2 == 0) {
                 int x = i < row ? i : row - 1;
                 int y = i < row ? 0 : i - row + 1;
@@ -24,7 +24,7 @@ public class Solution498 {
             } else {
                 int x = i < col ? 0 : i - col + 1;
                 int y = i < col ? i : col - 1;
-                while (x <= row && y >= 0) {
+                while (x < row && y >= 0) {
                     result[pos] = matrix[x][y];
                     x++;
                     y--;
@@ -40,6 +40,8 @@ public class Solution498 {
         int[][] m = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         Solution498 solution498 = new Solution498();
         System.out.println(Arrays.toString(solution498.findDiagonalOrder(m)));
+
+
     }
 
 
