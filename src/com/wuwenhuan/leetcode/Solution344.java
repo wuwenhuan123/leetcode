4df797c4bd847366d878d59c4f85c17d5b58c500
wwh <wwh960817@163.com>
2024-12-03 23:44:12 +0800

    完成题目28

---
+++ src/com/wuwenhuan/leetcode/Solution344.java
@@ -0,0 +1,13 @@
+package com.wuwenhuan.leetcode;
+
+public class Solution344 {
+
+    public void reverseString(char[] s) {
+        int n = s.length;
+        for (int left = 0, right = n - 1; left < right; ++left, --right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+        }
+    }
+}
